66c825c11a760f6081a907d6b0f4d846934e8760
Роман Быков <wvervolf@gmail.com>
2024-02-29 12:06:13 +0300

    HW

--- src/Book.java
+++ src/Book.java
@@ -1,16 +1,21 @@
 public class Book {
     private String title;
     private int year;
+    private Author name;
 
-    public Book(String title, int year) {
+    public Book(String title, int year, Author name) {
         this.title = title;
         this.year = year;
+        this.name = name;
     }
     public String getTitle() {
         return this.title;
     }
     public int getYear() {
         return this.year;
+    }
+    public Author getName() {
+        return this.name;
     }
     public void setYear(int year) {
         this.year = year;
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author("Андрей", "Андреев");
-        System.out.println("Автора зовут - " + author.getName() + " " + author.getSurname());
+        Author author = new Author("Андрей", "Андреев");//Оставил строку чисто для себя, поразбирать, че откуда берется
+        System.out.println("Автора зовут - " + author.getName() + " " + author.getSurname());//Оставил строку чисто для себя, поразбирать, че откуда берется
 
-        Book book = new Book("Археология", 2010);
+        Author aa = new Author("Андрей", "Андреев");
+        Book book = new Book("Археология", 2010, aa);
         book.setYear(2011);
-        System.out.println("Книга называется - " + book.getTitle() + ", она издана в " + book.getYear() + " году.");
+        System.out.println("Книга называется - " + book.getTitle() + ", она издана в " + book.getYear() + " году. А автора этой книги зовут " + author.getName() + " " + author.getSurname());
 
-        Author authorLiric = new Author(" Сергей", "Пупкин");
-        System.out.println("Автора зовут - " + authorLiric.getName() + " " + authorLiric.getSurname());
+        Author authorLiric = new Author(" Сергей", "Пупкин");//Оставил строку чисто для себя, поразбирать, че откуда берется
+        System.out.println("Автора зовут - " + authorLiric.getName() + " " + authorLiric.getSurname());//Оставил строку чисто для себя, поразбирать, че откуда берется
 
-        Book bookEnglish = new Book("История любви циклопа и дюймовочки", 2005);
+        Author sp = new Author("Сергей", "Пупкин");
+        Book bookEnglish = new Book("История любви циклопа и дюймовочки", 2005, sp);
         bookEnglish.setYear(2004);
-        System.out.println("Книга называется - " + bookEnglish.getTitle() + ", она издана в " + bookEnglish.getYear() + " году.");
+        System.out.println("Книга называется - " + bookEnglish.getTitle() + ", она издана в " + bookEnglish.getYear() + " году. А автора этой книги зовут " + author.getName() + " " + author.getSurname());
 
-        Author authorPhantastic = new Author("Александр", "Длиннохвостов");
-        System.out.println("Автора зовут - " + authorPhantastic.getName() + " " + authorPhantastic.getSurname());
+        Author authorPhantastic = new Author("Александр", "Длиннохвостов"); //Оставил строку чисто для себя, поразбирать, че откуда берется
+        System.out.println("Автора зовут - " + authorPhantastic.getName() + " " + authorPhantastic.getSurname());//Оставил строку чисто для себя, поразбирать, че откуда берется
 
-        Book bookPhantastic = new Book("Жизнь ихтиандров", 2001);
-        System.out.println("Книга называется - " + bookPhantastic.getTitle() + ", она издана в " + bookPhantastic.getYear() + " году.");
+        Author ad = new Author("Александр", "Длиннохвостов");
+        Book bookPhantastic = new Book("Жизнь ихтиандров", 2001, ad);
+        System.out.println("Книга называется - " + bookPhantastic.getTitle() + ", она издана в " + bookPhantastic.getYear() + " году. А автора этой книги зовут " + author.getName() + " " + author.getSurname());
     }
 }
